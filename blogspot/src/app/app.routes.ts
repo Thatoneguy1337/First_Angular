fcf2c28153189fb14762ebc067708adbbc6627ac
Victor Guterres <Vgb1337@gmail.com>
2024-03-26 14:16:51 -0300

    feat: Routes

--- blogspot/src/app/app.routes.ts
+++ blogspot/src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { RegisterComponent } from './pages/register/register.component';
 export const routes: Routes = [
 
     {path:"", component: HomeComponent},
-    {path:"/register", component: RegisterComponent},
-    {path:"/login", component: LoginComponent}
+    {path:"register", component: RegisterComponent},
+    {path:"login", component: LoginComponent}
 ];
