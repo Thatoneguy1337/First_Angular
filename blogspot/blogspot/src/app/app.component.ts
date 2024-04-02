b769fe96b3be4b519a4f9e5958ed1bcacb66d16a
Victor Guterres <Vgb1337@gmail.com>
2024-04-02 15:20:33 -0300

    feat: Header & Refactoring project

--- blogspot/blogspot/src/app/app.component.ts
+++ blogspot/blogspot/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
