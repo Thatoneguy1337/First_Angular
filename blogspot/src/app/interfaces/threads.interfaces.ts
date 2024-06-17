5d5f267c4c8dbd7328436bc8fc73964dc2816605
Victor Guterres <Vgb1337@gmail.com>
2024-06-17 10:40:01 -0300

    fix: thread interfaces

--- blogspot/src/app/interfaces/threads.interfaces.ts
+++ blogspot/src/app/interfaces/threads.interfaces.ts
@@ -1,19 +1,35 @@
-interface User {
+interface IUser {
     id: number;
     username: string;
     user_img: string;
 }
 
-interface Comment {
+interface IThread {
     id: number;
     created_at: string;
     edited: boolean;
     description: string;
     comment_img: string;
     username: string;
-    user: User;
+    user: IUser;
 }
 
+export interface IThreadData {
+    id: number | null;
+    created_at: string | null;
+    edited: boolean | null;
+    description: string | null;
+    comment_img: string | null;
+    username: string | null;
+    user: IUser;
+}
+
+
+
+
+export type TCreateThreadData = Omit<IThreadData, "id">;
+export type TEditThreadData = Partial<IThreadData> & { id: number };
+export type TThreadReturn = Omit<IThreadData, "comment_img">;
 
 
 
