b8be2b4d2b277beb64e106aa0cb92b06e5234b60
Victor Guterres <Vgb1337@gmail.com>
2024-06-20 11:11:29 -0300

    fix: formbuilder

--- blogspot/src/app/components/formlogin/formlogin.component.ts
+++ blogspot/src/app/components/formlogin/formlogin.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators, FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-formlogin',
   standalone: true,
@@ -8,18 +8,23 @@ import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angula
   templateUrl: './formlogin.component.html',
   styleUrl: './formlogin.component.css'
 })
-export class FormloginComponent {
-  loginForm = new FormGroup({
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(8),
-    ]),
-  });
+export class FormLoginComponent {
+  loginForm: FormGroup;
+  constructor(
+    private userService: UserService, 
+    private fb: FormBuilder 
+  ) {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
-  onSubmit() {
-    console.log(this.loginForm.get('password')?.errors);
-    console.log(this.loginForm.status);
-    this.loginForm.reset();
+  submit() {
+    if (this.loginForm.valid) {
+      const data = this.loginForm.value as TLoginUserData;
+      this.userService.login(data);
+      this.loginForm.reset();
+    }
   }
 }
--- blogspot/src/app/components/formregister/formregister.component.ts
+++ blogspot/src/app/components/formregister/formregister.component.ts
@@ -12,31 +12,37 @@ import {TRegisterUserData} from  '../../interfaces/user.interfaces';
 export class FormregisterComponent {
   constructor(
     private userService: UserService
-  ) {}
-  
-  registerForm = new FormGroup({
-   fullname:  new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(45)]),
-   username:  new FormControl(null,  [Validators.required, Validators.minLength(6)]) ,
-   user_img: new FormControl<string|null>(null),
-   bg_img: new FormControl<string|null>(null),
-   email: new FormControl<string|null>(null, [Validators.required, Validators.email]),
-   password: new FormControl<string|null>(null, [Validators.required, Validators.minLength(8)] ) ,
-   ssc_number: new FormControl<string|null>(null, [Validators.required, Validators.minLength(12), Validators.maxLength(12)]),
-   telephone: new FormControl<string|null>(null, [Validators.required, Validators.minLength(12)]),
-   birthdate: new FormControl<string|null>(null, [Validators.required, Validators.minLength(9)]),
-   zip_code: new FormControl<string|null>(null, [Validators.required, Validators.minLength(8)]),
-   state: new FormControl<string|null>(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
-   city: new FormControl<string|null>(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)] ),
-   street: new FormControl<string|null>(null, [Validators.required, Validators.minLength(5), Validators.maxLength(30)]),
-   number: new FormControl<string|null>(null, [Validators.required, Validators.minLength(1), Validators.maxLength(8)])
-  });
+    private fb: FormBuilder 
+  ) {
+
+    registerForm = new FormGroup({
+      fullname:  new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(45)]),
+      username:  new FormControl(null,  [Validators.required, Validators.minLength(6)]) ,
+      user_img: new FormControl<string|null>(null),
+      bg_img: new FormControl<string|null>(null),
+      email: new FormControl<string|null>(null, [Validators.required, Validators.email]),
+      password: new FormControl<string|null>(null, [Validators.required, Validators.minLength(8)] ) ,
+      ssc_number: new FormControl<string|null>(null, [Validators.required, Validators.minLength(12), Validators.maxLength(12)]),
+      telephone: new FormControl<string|null>(null, [Validators.required, Validators.minLength(12)]),
+      birthdate: new FormControl<string|null>(null, [Validators.required, Validators.minLength(9)]),
+      zip_code: new FormControl<string|null>(null, [Validators.required, Validators.minLength(8)]),
+      state: new FormControl<string|null>(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+      city: new FormControl<string|null>(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)] ),
+      street: new FormControl<string|null>(null, [Validators.required, Validators.minLength(5), Validators.maxLength(30)]),
+      number: new FormControl<string|null>(null, [Validators.required, Validators.minLength(1), Validators.maxLength(8)])
+     });
+   
+   
+     submit(){
+       const data = this.registerForm.value as TRegisterUserData;
+       this.userService.register(data);
+       this.registerForm.reset();
+     }
 
 
-  submit(){
-    const data = this.registerForm.value as TRegisterUserData;
-    this.userService.register(data);
-    this.registerForm.reset();
   }
+  
+ 
 
 
 }
