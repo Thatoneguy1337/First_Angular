2843799e2a1edfc3fa77842ccf034247d89311e9
victorguterres <vgb1337@gmail.com>
2024-06-27 15:05:06 -0300

    feat: added regex birthdate/telephone

--- blogspot/src/app/components/formregister/formregister.component.ts
+++ blogspot/src/app/components/formregister/formregister.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms'; 
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl, } from '@angular/forms'; 
 import { UserService } from '../../services/userService.services';
 import {TRegisterUserData} from  '../../interfaces/user.interfaces';
 @Component({
@@ -25,8 +25,8 @@ export class FormregisterComponent {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)] ) ,
       ssc_number: new FormControl(null, [Validators.required, Validators.minLength(12), Validators.maxLength(12)]),
-      telephone: new FormControl(null, [Validators.required, Validators.minLength(12)]),
-      birthdate: new FormControl(null, [Validators.required, Validators.minLength(9)]),
+      telephone: new FormControl(null, [Validators.required, Validators.minLength(12), Validators.pattern('^\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}$')]),
+      birthdate: new FormControl(null, [Validators.required, Validators.minLength(9), Validators.pattern('^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$')]),
       zip_code: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       state: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
       city: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)] ),
