eddae86b4fbbfa26c5b1d11f732a9e8b34c8bc9c
victorguterres <vgb1337@gmail.com>
2024-06-28 13:53:57 -0300

    feat: finished x task

--- blogspot/src/app/app.routes.ts
+++ blogspot/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent },
-    { path: "login", component: LoginComponent },
+    { path: "", component: HomeComponent, canActivate: [authGuard] },
+    { path: "login", component: LoginComponent},
     { path: "register", component: RegisterComponent }
-
 ];
