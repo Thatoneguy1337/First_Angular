268638378d2480ba15be15af9e791c3132448069
victorguterres <vgb1337@gmail.com>
2024-07-02 12:35:09 -0300

    fix: header mobile version

--- blogspot/src/app/components/header/header.component.ts
+++ blogspot/src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive ],
+  imports: [RouterLink, RouterLinkActive, CommonModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isNavVisible = false;
 
+  toggleNav() {
+    this.isNavVisible = !this.isNavVisible;
+  }
 }
