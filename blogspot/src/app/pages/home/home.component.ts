6451b9657a6448a69d8941b70321ae496ff83cd1
victorguterres <vgb1337@gmail.com>
2024-07-09 10:32:27 -0300

    feat: Aside

--- blogspot/src/app/pages/home/home.component.ts
+++ blogspot/src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AsideComponent } from '../../components/aside/aside.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AsideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
